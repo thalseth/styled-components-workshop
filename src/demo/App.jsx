e187ab32d21be35bf63159f2b264450be3670b23
Torleif Halseth <halseth.torleif@gmail.com>
2018-09-28 08:07:01 +0200

    Remove unused div

--- src/demo/App.jsx
+++ src/demo/App.jsx
@@ -3,6 +3,7 @@ import styled, { ThemeProvider } from 'styled-components';
 import GlobalStyle from './GlobalStyle';
 import Button, { TextButton } from './Button';
 import { ShadowCard, CardImage } from './Card';
+// eslint-disable-next-line
 import { normal, healthy } from '../themes';
 
 const AppStyled = styled.div``;
@@ -49,51 +50,47 @@ const Cards = styled.div`
 export default () => (
   <ThemeProvider theme={normal}>
     <AppStyled>
-      <div className="App">
-        <Header>
-          <h1>Styled components workshop</h1>
-        </Header>
-        <Section>
-          <h2>Buttons</h2>
-          <Button onClick={() => alert('My first styled button!')}>
-            Button
-          </Button>
-          <Button as="a" href="http://www.knowit.no">
-            Button - Anchor
-          </Button>
-          <Button as="a" href="http://www.knowit.no" size="large">
-            Button - Large
-          </Button>
-          <TextButton as="a" href="http://www.knowit.no">
-            TextButton
-          </TextButton>
-        </Section>
-        <Section>
-          <h2>Cards</h2>
-          <Cards>
-            <ShadowCard hasImage>
-              <CardImage image="https://picsum.photos/5512/3708?image=1081" />
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-              <TextButton onClick={() => alert('My first styled button!')}>
-                Learn more
-              </TextButton>
-            </ShadowCard>
-            <ShadowCard hasImage>
-              <CardImage image="https://picsum.photos/5616/3744?image=1048" />
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-              <TextButton onClick={() => alert('My first styled button!')}>
-                Learn more
-              </TextButton>
-            </ShadowCard>
-          </Cards>
-        </Section>
-      </div>
+      <Header>
+        <h1>Styled components workshop</h1>
+      </Header>
+      <Section>
+        <h2>Buttons</h2>
+        <Button onClick={() => alert('My first styled button!')}>Button</Button>
+        <Button as="a" href="http://www.knowit.no">
+          Button - Anchor
+        </Button>
+        <Button as="a" href="http://www.knowit.no" size="large">
+          Button - Large
+        </Button>
+        <TextButton as="a" href="http://www.knowit.no">
+          TextButton
+        </TextButton>
+      </Section>
+      <Section>
+        <h2>Cards</h2>
+        <Cards>
+          <ShadowCard hasImage>
+            <CardImage image="https://picsum.photos/5512/3708?image=1081" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <TextButton onClick={() => alert('My first styled button!')}>
+              Learn more
+            </TextButton>
+          </ShadowCard>
+          <ShadowCard hasImage>
+            <CardImage image="https://picsum.photos/5616/3744?image=1048" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <TextButton onClick={() => alert('My first styled button!')}>
+              Learn more
+            </TextButton>
+          </ShadowCard>
+        </Cards>
+      </Section>
       <GlobalStyle />
     </AppStyled>
   </ThemeProvider>
